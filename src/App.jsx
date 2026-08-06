407d0e1a0b7efe796cc63623482433b2bc5977ea
KwonKiSeong <rltjd2313@naver.com>
2026-08-06 10:15:01 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const API_BASE_URL = 'http://localhost:8080/api/boards';
+const API_BASE_URL = "http://52.79.251.33:8080";
 
 export default function App() {
   const [boards, setBoards] = useState([]);
